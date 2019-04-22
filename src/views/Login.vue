81f7d697a4c0cac75a2bdb2b56e8f56a7ca5fe9d
AkronimBlack <bojanmadzaracka@gmail.com>
2019-04-22 13:43:34 +0200

    login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,31 @@
 <template>
     <v-container fluid fill-height>
         <v-layout justify-center align-center>
-            Login
+            <v-flex xs12 sm8 md4>
+                <v-card class="elevation-12">
+                    <v-toolbar dark color="primary">
+                        <v-toolbar-title>Login form</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field prepend-icon="person" name="login" label="Login" type="text"></v-text-field>
+                            <v-text-field prepend-icon="lock" name="password" label="Password" id="password"
+                                          type="password"></v-text-field>
+                        </v-form>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary">Login</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex>
         </v-layout>
     </v-container>
 </template>
 
 <script>
-    import { mapGetters, mapActions } from "vuex";
+    import {mapGetters, mapActions} from "vuex";
+
     export default {
         name: "login",
         data() {
